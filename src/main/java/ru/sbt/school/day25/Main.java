79855fe93848a2ba9a273e85ae7e46ff1369c068
Alex <chas7610@gmail.com>
2018-08-09 16:20:23 +0300

    day25

--- src/main/java/ru/sbt/school/day25/Main.java
+++ src/main/java/ru/sbt/school/day25/Main.java
@@ -9,26 +9,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         CookingRecipeService service = new CookingRecipeService();
-
-//        List<CookingRecipe> list = service.getAll();
-//        list.stream().forEach(System.out::println);
-
-//        List<CookingRecipe> list = service.getbyIngredient("pineapple");
-//        list.stream().forEach(System.out::println);
-
-//        String lines = "salad_Young, red potatoes:250; grean pineapple:100; young chicken:50;mayonnaise:5,I";
-//        service.insertRow(lines);
-//        service.getAll().stream().forEach(System.out::println);
-
-//        service.deleteRowById(45);
-//        service.getAll().stream().forEach(System.out::println);
-
+//        get all rows
+        List<CookingRecipe> list = service.getAll();
+        list.stream().forEach(System.out::println);
+//        get row(s) by name
+        service.getByName("salad_Young").stream().forEach(System.out::println);
+//        get row by ingrediant %like%
+        service.getbyIngredient("pineapple").stream().forEach(System.out::println);
+//        insert row
+        String lines = "salad_Young, red potatoes:250; grean pineapple:100; young chicken:50;mayonnaise:5,I";
+        service.insertRow(lines);
+        service.getAll().stream().forEach(System.out::println);
+//        delete row by id
+        service.deleteRowById(45);
+        service.getAll().stream().forEach(System.out::println);
+//        delete row(s) by name
         System.out.println(service.deleteRowByName("salad_Young"));
-
-//        service.getByName("salad_Young").stream().forEach(System.out::println);
-
-//        System.out.println(service.deleteRowByName("salad_Young"));
-//        service.getAll().stream().forEach(System.out::println);
+        service.getAll().stream().forEach(System.out::println);
 
         HibernateSessionFactoryUtil.close();
     }
